fa9599fb5d16632cb9518c7d73fa0ad2a996c6e5
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-16 23:27:57 +0200

    Removed console logs

--- script.js
+++ script.js
@@ -1,65 +1,50 @@
 //1-root = document(order1),
 const root1 = document.querySelector('[data-key="order1"]')
-console.dir(root1)
 
 // const 1-biscuits: document(biscuits),
 // const biscuits1 = document.querySelector('[class="biscuits"]')
 const biscuits1 = root1.querySelector('[class="biscuits"]')
-console.dir(biscuits1)
 
 // const 1-donuts: document(donuts)
 //const donuts1 = document.querySelector('[class="donuts"]')
 const donuts1 = document.querySelector('[class="donuts"]')
-console.dir(donuts1)
 
 // const 1-pancakes: document(pancakes)
 const pancakes1 = root1.querySelector('[class="pancakes"]')
-console.dir(pancakes1)
 
 // const 1-status: document(status)
 // const status1 = document.querySelector('[class="status"]')
 const status1 = root1.parentElement.querySelector('[class="status"]')
-console.dir(status1)
 
 // const 2-root = document(order2),
 const root2 = document.querySelector('[data-key="order2"]')
-console.dir(root2)
 
 // const 2-biscuits: document(biscuits),
 const biscuits2 = root2.querySelector('[class="biscuits"]')
-console.dir(biscuits1)
 
 // const 2-donuts: document(donuts),
 const donuts2 = root2.querySelector('[class="donuts"]')
-console.dir(donuts2)
 
 // const 2-pancakes: document(pancakes),
 const pancakes2 = root2.querySelector('[class="pancakes"]')
-console.dir(pancakes2)
 
 // const 2-status: document(status)
 const status2 = root2.parentElement.querySelector('[class="status"]')
-console.dir(status2)
 
 // const 3-root = document(order3),
 const root3 = document.querySelector('[data-key="order3"]')
-console.dir(root3)
 
 // const 3-biscuits: document(biscuits),
 const biscuits3 = root3.querySelector('[class="biscuits"]')
-console.dir(biscuits3)
 
 // const 3-donuts: document(donuts),
 const donuts3 = root3.querySelector('[class="donuts"]')
-console.dir(donuts3)
 
 // const 3-pancakes: document(pancakes),
 const pancakes3 = root3.querySelector('[class="pancakes"]')
-console.dir(pancakes3.innerHTML)
 
 // const 3-status: document(status)
 const status3 = root3.parentElement.querySelector('[class="status"]')
-console.dir(status3)
 
 
 // 1-biscuits= 1-root.biscuits,
@@ -90,9 +75,7 @@ pancakes3.querySelector('[class="count"]').textContent = root3.dataset.pancakes
 status3.textContent = Boolean(root3.dataset.delivered) ? "Delivered" : "Pending"
 
 // document(biscuits.p-count).pending = 3-status ? 1-biscuits + 2-biscuits + 3-biscuits: 0
-document(biscuits.p-count).pending = 3-status ? biscuits1 + biscuits2 + biscuits3 : 0
 // document(biscuits.p-count).delivered 3-status ? 0 : 1-biscuits + 2-biscuits + 3-biscuits
-document(biscuits.p-count).delivered 3-status ? 0 : biscuits1 + biscuits2 + biscuits3
 
 // document(donuts.p-count).pending = 3-status ? 1-donuts + 2-donuts + 3-donuts: 0
 // document(donuts.p-count).delivered 3-status ? 0 : 1-donuts + 2-donuts + 3-donuts
